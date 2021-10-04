773bea1e1e95791811ebddceeb1db4c91686a058
Dolan <dolanl@iu.edu>
2021-10-04 10:55:56 -0400

    connected df concat to pybliometrics

--- subject_area_test.py
+++ subject_area_test.py
@@ -1,11 +1,49 @@
-from pybliometrics.scopus import AuthorRetrieval
 import pandas as pd
+from pybliometrics.scopus import AuthorSearch, AuthorRetrieval
 
-df = pd.read_csv(r'C:\Users\Levi\source\repos\Scopus_API\test_authors.csv')
+df = pd.read_csv(r'C:\Users\dolanl\Projects\Scopus_API\a_test.csv')
 
-#for i in df['Scopus_ID']:
-    #au = AuthorRetrieval(i)
-    #print(au)
+#pd.set_option('display.max_columns', None)
+#df_names['Scopus_ID'] = df_names.apply(AuthorSearch('AUTHLAST(Faculty Last Name[])' and 'AUTHFIRST(Faculty First Name[])'))
 
-print(AuthorRetrieval(6505808971))
+#find_auth = AuthorSearch('AUTHLAST(Hardacker) and AUTHFIRST(Doris)')
+
+#print(find_auth)
+
+#au = AuthorRetrieval(6505808971)
+#for index, row in df.iterrows():
+    #au = AuthorSearch('AUTHLAST(' + row['Faculty Last Name'] + ') and AUTHFIRST(' + row['Faculty First Name'] + ')')
+#print(au)    
+
+au_list = [6505808971,6506552389,6602261254]
+
+def get_info(scopus_id):
+    au = AuthorRetrieval(scopus_id)
+    df1 = pd.DataFrame(au.affiliation_current)
+    #df2 = pd.DataFrame(au.affiliation_history)
+    df3 = pd.DataFrame(au.subject_areas)
+    frames = [df1, df3]
+    df_all = pd.concat(frames)
+
+    return df_all
+
+au = AuthorRetrieval(6505808971)
+
+#print('Citations: ', au.citation_count, 'Documents: ', au.document_count, 'h-index: ', au.h_index, 'ORCID: ', au.orcid, 
+#'Publication Range: ', au.publication_range)
+#test = get_info(6505808971)
+
+#test.to_csv(r'C:\Users\dolanl\Projects\Scopus_API\test2.csv')
+#print(au.get_coauthors())
+#au = AuthorRetrieval(6602261254, view='ENHANCED')
+#coauthors = pd.DataFrame(au.get_coauthors())
+#docs = pd.DataFrame(au.get_documents(refresh=7)
+
+
+docs = pd.DataFrame(au.get_documents())
+
+docs.to_csv(r'C:\Users\dolanl\Projects\Scopus_API\docs2.csv')
+ 
+    #pd.set_option('display.max_columns', None)
+    
 
